7a2bdb590f410da63e56a7409df99ce874e97a4e
Julio Dalpiaz <juliodalpiaz@gmail.com>
2026-03-31 22:46:38 -0400

    fix(auth): improve JWT middleware structure

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,18 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
     const token = req.headers.authorization;
+
     if(!token){
-        return res.status(401).json({ message: 'Acesso Negado! '})
+        return res.status(401).json({ message: 'Acesso Negado! '});
     }
-    next();
+
+    try{
+        const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET); //o replace é para retirar a palavra "Bearer " do token, caso ela exista
+        req.user = decoded;
+        next(); //deixar dentro do try por segurança
+    }catch(err){
+        return res.status(401).json({ message: 'Token Inválido!' });
+    }
 }
 
 module.exports = auth;
